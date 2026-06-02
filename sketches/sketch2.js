acdb15b5965c45784e95602e3f2c3b578b137b75
spencer-m8 <shsmclean@gmail.com>
2026-06-01 21:44:37 -0400

    added sketch 1 and updated credits

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,13 +1,42 @@
-// =============================================
-// sketch2.js — your second source sketch
-// Paste your source sketch code here and start hacking
-// =============================================
+// by SamuelYAN
+// more works //
+// https://twitter.com/SamuelAnn0924
+// https://www.instagram.com/samuel_yan_1990/
+
+let mySize;
+
+// a shader variable
+let theShader;
+
+function preload() {
+	theShader = new p5.Shader(this.renderer, vert, frag)
+}
 
 function setup() {
-  createCanvas(800, 500);
+	mySize = min(windowWidth, windowHeight) * 1.0;
+	// shaders require WEBGL mode to work
+	createCanvas(mySize / 16 * 11, mySize, WEBGL);
+	noStroke();
 }
 
 function draw() {
-  background(220);
-  // your code here
+	// shader() sets the active shader with our shader
+	shader(theShader);
+
+	theShader.setUniform("u_resolution", [width, height]);
+	theShader.setUniform("u_time", millis() / 1000.0);
+	theShader.setUniform("u_frame", frameCount / 10.0);
+	theShader.setUniform("u_mouse", [mouseX / 100.0, map(mouseY, 0, height, height, 0) / 100.0]);
+
+	// rect gives us some geometry on the screen
+	rect(0, 0, width, height);
 }
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+// by SamuelYAN
+// more works //
+// https://twitter.com/SamuelAnn0924
+// https://www.instagram.com/samuel_yan_1990/
